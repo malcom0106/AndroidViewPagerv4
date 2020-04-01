058cc26730f2412d87f2fa0723cad5b65b71b23d
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-04-01 10:52:21 +0200

    PagerTabStrip ajouté au layout

--- app/src/main/java/com/example/exemplepagerview/MainActivity.java
+++ app/src/main/java/com/example/exemplepagerview/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         //On instencie nos fragments
         mainFragment = new MainFragment();
         fragment1 = new Fragment1();
